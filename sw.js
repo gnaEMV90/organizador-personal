1911d03795e146e642007b1acee1f29ed67f6773
German Andrighetti <85268545+gnaEMV90@users.noreply.github.com>
2026-07-13 14:03:56 -0300

    Renombrar caché PWA a Planora

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'organizador-v1';
+const CACHE_NAME = 'planora-v1';
 const APP_SHELL = ['/', '/index.html', '/styles.css', '/app.js', '/manifest.webmanifest', '/icons/icon.svg', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', event => {
